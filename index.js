43a1be4f02e763ca078891fa3c2da9dd1c4faa16
Carrie Mathieu <carrieburks@gmail.com>
2021-03-28 01:14:43 -0400

    added category display to index.js

--- index.js
+++ index.js
@@ -1,1 +1,22 @@
-console.log("in index.js")
+categories_url = "http://localhost:3000/api/categories"
+
+document.addEventListener('DOMContentLoaded', () => {
+    getCategories()
+})
+
+function getCategories() {
+    fetch(categories_url)
+    .then(resp => resp.json())
+    .then(categories => {
+        categories.data.forEach( category => {
+            const categoryMarkup = 
+            `<div data-id=${category.id}>
+            <h3> ${category.attributes.name} </h3>`
+
+            document.querySelector('#category-container').innerHTML += categoryMarkup
+        }
+        )
+        
+    }
+    )
+}
