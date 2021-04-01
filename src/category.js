27df1eb73ddd28a8f58eccfacc8fd005dc035e7c
Carrie Mathieu <carrieburks@gmail.com>
2021-04-01 19:12:45 -0400

    added event listener for cat dropdown to pull up word lists

---
+++ src/category.js
@@ -0,0 +1,11 @@
+class Category {
+    constructor(name) {
+        this.name = name
+        Category.all.push(this)
+    }
+
+    // to render category dropdown & dynamically add
+
+}
+
+Category.all = []
